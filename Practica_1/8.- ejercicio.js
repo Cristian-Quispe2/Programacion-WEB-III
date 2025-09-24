8afa99288b903c85648bb8a9c10469aaa7b8aa9b
Cristian Quispe <cq34073@gmail.com>
2025-09-23 20:56:02 -0400

    8.- ejercicio.js

---
+++ Practica_1/8.- ejercicio.js
@@ -0,0 +1,6 @@
+// Callback y Promesas en JS
+// 8. Realizar un código para ejecutar una función callback después 2 segundos.
+function Callback() {
+    console.log("funcion ejecutada.");
+}
+setTimeout(Callback, 2000);
