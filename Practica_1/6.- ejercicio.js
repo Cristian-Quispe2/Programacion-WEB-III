63d785d403bf8c29707f651339dcd2df4e2fc948
Cristian Quispe <cq34073@gmail.com>
2025-09-23 20:54:04 -0400

    6.- ejercicio.js

---
+++ Practica_1/6.- ejercicio.js
@@ -0,0 +1,5 @@
+// 6.- Tomar los dos primeros elementos de un arreglo y almacenarlos en dos variables mediante desestructuración.
+const arreglo = [5, -15, 25, 8];
+const [primero, segundo] = arreglo;
+console.log(primero); // 10
+console.log(segundo); // 20
